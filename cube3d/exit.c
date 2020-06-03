3a77dd82c00e9e806c71a158bbc28a4c863dfd11
Sosthene00 <674694@protonmail.ch>
2020-06-03 15:01:41 +0200

    Add ERROR_PARSING case in the error message

--- cube3d/exit.c
+++ cube3d/exit.c
@@ -4,6 +4,8 @@ void    print_error(int error_code)
 {
     if (error_code == 1)
         ft_putstr_fd(WRONG_FILE, 2);
+    else if (error_code == 2)
+        ft_putstr_fd(ERROR_PARSING, 2);
     else
         ft_putstr_fd("Unknown error code\n", 2);
 }
