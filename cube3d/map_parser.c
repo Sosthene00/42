ae88f2320f0812d9a19d786c5fbfd13a9ffe2db1
Sosthene00 <674694@protonmail.ch>
2020-06-03 15:00:09 +0200

    Separate get_xy_value function to get the resolution

--- cube3d/map_parser.c
+++ cube3d/map_parser.c
@@ -1,50 +1,61 @@
 # include "./cube3d.h"
 
-char *get_identifier(char *line)
+int get_identifier(char *line, char *identifier)
 {
-    char *identifier;
+    // return either 2 or -1
+    ft_bzero(identifier, 4);
+    if (!(ft_memccpy(identifier, line, ' ', 3)))
+        return (-1);
+    return (2);
+}
 
-    if (!(identifier = malloc(sizeof(*identifier)*3)))
-        return (NULL);
-    ft_bzero(identifier, 3);
-    ft_memccpy(identifier, line, 32, 2);
-    return (identifier);
+int get_xy_value(char *line, map *map_data)
+{
+    int i;
+    char tmp[5];
+
+    while (*line && ((map_data->x == 0) || (map_data->y == 0)))
+    {
+        i = 0;
+        while (*line == ' ')
+            line++;
+        while ((ft_isdigit(*line)) != 0)
+        {
+            tmp[i] = *line;
+            i++;
+            line++;
+        }
+        tmp[i] = '\0';
+        if (map_data->x == 0)
+            map_data->x = ft_atoi(tmp);
+        else
+            map_data->y = ft_atoi(tmp);
+        
+    }
+    return (0);
 }
 
 int update_data(char *line, map *map_data)
 {
+    // Take a line, parse the identifier and dispatch the content to the right function 
+    // to update map_data
     char *identifier;
-    char temp[BUFFER_SIZE + 1];
     int i;
 
-    identifier = get_identifier(line);
+    if (!(identifier = ft_calloc(4, sizeof(*identifier))))
+        return (2);
+    if ((i = get_identifier(line, identifier) > 0))
+        line = line + i;
+    else
+        return (2);
+    
     if ((identifier[0] == 'R') && (identifier[1] == ' '))
     {
-        while (*++line)
-        {
-            i = 0;
-            while (*line == 32)
-                line++;
-            while ((ft_isdigit(*line)) != 0)
-            {
-                temp[i] = *line;
-                i++;
-                line++;
-            }
-            temp[i] = '\0';
-            map_data->x = ft_atoi(temp);
-            i = 0;
-            while (*line == 32)
-                line++;
-            while ((ft_isdigit(*line)) != 0)
-            {
-                temp[i] = *line;
-                i++;
-                line++;
-            }
-            map_data->y = ft_atoi(temp);
+        // Parse x and y value from file, return 0 if ok, 2 otherwise
+        if (get_xy_value(line, map_data) == 0)
             return (0);
-        }
+        else
+            return (2);
     }
     return (2);
 }
@@ -56,13 +67,18 @@ int parse_map_file(char *map_file, map *map_data)
     int ret;
     char *line;
 
-    if ((map_file == NULL) || (fd = open(map_file, O_RDONLY)) < 0)
+    //if ((map_file == NULL) || (fd = open(map_file, O_RDONLY)) < 0)
+    if ((fd = open(map_file, O_RDONLY)) < 0)
         return (1);
     map_data->name = map_file;
     while (get_next_line(fd, &line) > 0)
     {
         if (ft_isalpha(line[0]))
+        {
             ret = update_data(line, map_data);
+            if (ret == 2)
+                break;
+        }
         else
             continue;
     }
