e245410cded3bad466c66ca0c6fc85d6905d8403
Sosthene00 <674694@protonmail.ch>
2020-06-03 23:29:33 +0200

    Replace exit.c by error.c

--- cube3d/error.c
+++ cube3d/error.c
@@ -6,15 +6,17 @@ void    print_error(int error_code)
         ft_putstr_fd(WRONG_FILE, 2);
     else if (error_code == 2)
         ft_putstr_fd(ERROR_PARSING, 2);
+    else if (error_code == 9)
+        ft_putstr_fd(MEMORY_ERROR, 2);
     else
         ft_putstr_fd("Unknown error code\n", 2);
 }
 
-int     exit_program(void *mlx_ptr, void *win_ptr, int error_code)
+void     exit_program(ctx *context, int error_code)
 {
     if (error_code != 0)
         print_error(error_code);
-    mlx_clear_window(mlx_ptr, win_ptr);
-    mlx_destroy_window(mlx_ptr, win_ptr);    
-    return (error_code);
+    mlx_clear_window(context->mlx_ptr, context->win_ptr);
+    mlx_destroy_window(context->mlx_ptr, context->win_ptr);    
+    exit(1);
 }
