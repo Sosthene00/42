40c4ead34337094cda19ae4373073bbeaef19bfc
Sosthene00 <674694@protonmail.ch>
2020-06-03 22:11:50 +0200

    Add is_in_screen_size function

--- cube3d/util.c
+++ cube3d/util.c
@@ -9,3 +9,14 @@ map *init_map_data()
     bzero(map_data, sizeof(*map_data));
     return (map_data);
 }
+int is_in_screen_size(void *mlx_ptr, int x, int y)
+{
+  int *max_x;
+  int *max_y;
+
+  mlx_get_screen_size(mlx_ptr, max_x, max_y);
+  if ((x <= max_x) && (y <= max_y))
+    return (1);
+  else
+    return (0);
+}
