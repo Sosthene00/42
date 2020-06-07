c77f4155991a4a207513b3f5ff1808aa7a4f8c32
Sosthene00 <674694@protonmail.ch>
2020-06-07 22:21:13 +0200

    Prevent crashing when the program exit with an error while no window has been created yet

--- cube3d/error.c
+++ cube3d/error.c
@@ -9,7 +9,7 @@ void    print_error(int error_code)
     else if (error_code == 9)
         ft_putstr_fd(MEMORY_ERROR, 2);
     else
-        ft_putstr_fd("Unknown error code\n", 2);
+        ft_putstr_fd("Unknown error\n", 2);
 }
 
 int check_file_extension(char *filename)
@@ -32,7 +32,12 @@ void     exit_program(ctx *context, int error_code)
     if (error_code != 0)
         print_error(error_code);
     //mlx_do_key_autorepeaton(context->mlx_ptr);
-    mlx_clear_window(context->mlx_ptr, context->win_ptr);
+    if (context->win_ptr != NULL)
+    {
+        mlx_clear_window(context->mlx_ptr, context->win_ptr);
+        mlx_destroy_window(context->mlx_ptr, context->win_ptr);    
     mlx_destroy_window(context->mlx_ptr, context->win_ptr);    
+        mlx_destroy_window(context->mlx_ptr, context->win_ptr);    
+    }
     exit(error_code);
 }
