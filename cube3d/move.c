7318980fb3c28c57e2dff7800c9c0b3b5756948c
Sosthene00 <674694@protonmail.ch>
2020-06-08 16:42:21 +0200

    Create move.c

---
+++ cube3d/move.c
@@ -0,0 +1,13 @@
+#include "cube3d.h"
+
+void    move_up(ctx *context)
+{
+    print_whole_screen(0xffffff, context);
+    context->player.move_up = 0;
+}
+
+void    move_down(ctx *context)
+{
+    print_whole_screen(0x000001, context);
+    context->player.move_down = 0;
+}
