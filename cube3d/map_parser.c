6a297788e9c7c966d03e6c8ab36a5a6bedd991a2
Sosthene00 <674694@protonmail.ch>
2020-06-08 22:02:05 +0200

    Add parsing for the ceiling and front color

--- cube3d/map_parser.c
+++ cube3d/map_parser.c
@@ -1,5 +1,17 @@
 # include "./cube3d.h"
 
+unsigned int    convert_color(int r, int g, int b)
+{
+    unsigned int color;
+
+    if ((r < 0) || (r > 255) ||\
+        (g < 0) || (g > 255) ||\
+        (b < 0) || (b > 255))
+        return (pow(2, 32) - 1);
+    color = (r << 16) + (g << 8) + b;
+    return (color);
+}
+
 int get_xy_value(char *line, ctx *context)
 {
     char **val;
@@ -15,6 +27,29 @@ int get_xy_value(char *line, ctx *context)
         return (2);
 }
 
+int get_color(char *line, ctx *context)
+{
+    char **val;
+    char identifier;
+    unsigned int color;
+
+    identifier = *line;
+    while (!(ft_isdigit(*line)))
+        line++;
+    val = ft_split(line, ',');
+    if (!(val[0]) || !(val[1]) || !(val[2]) || \
+        !(ft_isdigit(*val[0])) || !(ft_isdigit(*val[1])) || !(ft_isdigit(*val[2])))
+        return (2);
+    color = convert_color(ft_atoi(val[0]), ft_atoi(val[1]), ft_atoi(val[2]));
+    if (color == (pow(2, 32) - 1))
+        return (2);
+    if (identifier == 'C')
+        context->color.color_sky = color;
+    else if (identifier == 'F')
+        context->color.color_floor = color;
+    return (0);
+}
+
 int update_data(char *line, ctx *context)
 {
     // Take a line, parse the identifier and dispatch the content to the right function 
@@ -26,8 +61,11 @@ int update_data(char *line, ctx *context)
             init_win(context);
             return (0);
         }
-        else
-            return (2);
+    }
+    else if ((ft_strncmp(line, CEILING, 2) == 0) || (ft_strncmp(line, FLOOR, 2) == 0))
+    {
+        if (get_color((line), context) == 0)
+            return (0);
     }
     return (2);
 }
