35a8e13f17bb48a3bc04cbec1f57acc67d1b1887
Sosthene00 <674694@protonmail.ch>
2020-06-13 13:38:44 +0200

    Implement the logic of moving up and down

--- cube3d/move.c
+++ cube3d/move.c
@@ -2,12 +2,22 @@
 
 void    move_up(ctx *context)
 {
-    print_whole_screen(context->color.color_floor, context);
+    if (context->map[(int)(context->player.pos.x + context->player.dir.x * context->player.speed_move)]
+            [(int)context->player.pos.y] == '0')
+        context->player.pos.x += context->player.dir.x * context->player.speed_move;
+    if (context->map[(int)context->player.pos.x][(int)(context->player.pos.y + context->player.dir.y * 
+            context->player.speed_move)] == '0')
+        context->player.pos.y += context->player.dir.y * context->player.speed_move;
 }
 
 void    move_down(ctx *context)
 {
-    print_whole_screen(context->color.color_sky, context);
+    if (context->map[(int)(context->player.pos.x - context->player.dir.x * context->player.speed_move)]
+            [(int)context->player.pos.y] == '0')
+        context->player.pos.x -= context->player.dir.x * context->player.speed_move;
+    if (context->map[(int)context->player.pos.x][(int)(context->player.pos.y - context->player.dir.y * 
+            context->player.speed_move)] == '0')
+        context->player.pos.y -= context->player.dir.y * context->player.speed_move;
 }
 
 void    stop(ctx *context)
