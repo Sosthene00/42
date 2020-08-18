61294c65fe98d076aeacab20a20aa1524f472664
Sosthene00 <674694@protonmail.ch>
2020-08-18 16:19:31 +0200

    Add get_bitmap_header

--- cube3d/save_bmp.c
+++ cube3d/save_bmp.c
@@ -1,5 +1,15 @@
 # include "cube3d.h"
 
+void    get_bitmap_header(bmp *file, img *screen)
+{
+    file->PixelDataOffset = 54;
+    file->HeaderSize = 40;
+    file->ImageWidth = screen->width;
+    file->ImageHeight = screen->height;
+    file->Planes = 1;
+    file->BitsPerPixel = (unsigned short)screen->bits_per_pixel;
+}
+
 int save_screenshot(ctx *c)
 {
     int fd;
@@ -11,7 +21,7 @@ int save_screenshot(ctx *c)
         exit_program(c, 3);
     if (!(file = ft_calloc(1, sizeof(*file))))
         exit_program(c, 9);
-    //get_bitmap_header(file, &(c->screen));
+    get_bitmap_header(file, &(c->screen));
     /*if (write_to_disk(fd, file, c->screen) < 0)
         exit_program(c, 3);*/
     if (close(fd) < 0)
